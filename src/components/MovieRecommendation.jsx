ce8d943374120b85595342a8718e49d285a00e4f
Srinjoy Das <162009749+Troy212@users.noreply.github.com>
2024-11-07 21:30:57 +0530

    Update MovieRecommendation.jsx

--- src/components/MovieRecommendation.jsx
+++ src/components/MovieRecommendation.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+
 function MovieRecommendation() {
   const [movies, setMovies] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
